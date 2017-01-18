f8b071704dc08f3400ebcac6405470280118a9af
John Mergy <johnmergy@Johns-MacBook-Air.local>
2017-01-18 16:39:37 -0500

    revisions

---
+++ projects/js_library/0.02_json.js
@@ -0,0 +1,10 @@
+
+{"employees": [
+    {"firstName":"John", "lastName":"Doe"}, 
+    {"firstName":"Anna", "lastName":"Smith"},
+    {"firstName":"Peter", "lastName":"Jones"}
+]
+}
+console.log("employees")
+
+/*TODO make this work*/
